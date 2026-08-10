27ce3f5748cd6585bfe516d34d5ef2a6bbf51c8e
Dyorgenes <dyorgenes2018@gmail.com>
2026-08-10 17:44:36 -0300

    implementação da camada de dominio.

---
+++ src/main/java/santander_dev_week_2023/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package santander_dev_week_2023.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import santander_dev_week_2023.model.User;
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long> {
+
+}
